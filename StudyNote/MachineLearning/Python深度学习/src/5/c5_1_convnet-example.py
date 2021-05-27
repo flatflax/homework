4596c5d14c28662861ee3f7e5716d68b1571e2d1
Flax <present_xm@qq.com>
2021-05-27 21:46:24 +0800

    update chapter 5

--- StudyNote/MachineLearning/Python深度学习/src/5/c5_1_convnet-example.py
+++ StudyNote/MachineLearning/Python深度学习/src/5/c5_1_convnet-example.py
@@ -1,3 +1,6 @@
+"""
+构建一个简单的卷积网络样例
+"""
 from keras import layers
 from keras import models
 
@@ -7,6 +10,13 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu')) 
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# 分类器，将3D张量压缩为1D张量输出
+model.add(layers.Flatten()) 
+model.add(layers.Dense(64, activation='relu')) 
+model.add(layers.Dense(10, activation='softmax'))
+
+print(model.summary())
 
 """
 Model: "sequential"
@@ -22,8 +32,15 @@ _________________________________________________________________
 max_pooling2d_1 (MaxPooling2 (None, 5, 5, 64)          0         
 _________________________________________________________________
 conv2d_2 (Conv2D)            (None, 3, 3, 64)          36928     
+_________________________________________________________________
+flatten (Flatten)            (None, 576)               0         
+_________________________________________________________________
+dense (Dense)                (None, 64)                36928     
+_________________________________________________________________
+dense_1 (Dense)              (None, 10)                650       
 =================================================================
-Total params: 55,744
-Trainable params: 55,744
+Total params: 93,322
+Trainable params: 93,322
 Non-trainable params: 0
+_________________________________________________________________
 """
